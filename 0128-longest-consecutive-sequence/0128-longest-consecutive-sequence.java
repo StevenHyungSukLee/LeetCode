0545642f309b4dd01a8c1322d2d12c0152b0af0c
StevenHyungSukLee <106793032+StevenHyungSukLee@users.noreply.github.com>
2022-12-18 21:30:40 -0500

    Time: 473 ms (31.15%), Space: 152.7 MB (8.61%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,26 +1,51 @@
 class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         Set<Integer> num_set = new HashSet<Integer>();
+//         for (int num : nums) {
+//             num_set.add(num);
+//         }
+
+//         int longestStreak = 0;
+
+//         for (int num : num_set) {
+//             if (!num_set.contains(num-1)) {
+//                 int currentNum = num;
+//                 int currentStreak = 1;
+
+//                 while (num_set.contains(currentNum+1)) {
+//                     currentNum += 1;
+//                     currentStreak += 1;
+//                 }
+
+//                 longestStreak = Math.max(longestStreak, currentStreak);
+//             }
+//         }
+
+//         return longestStreak;
+//     }
     public int longestConsecutive(int[] nums) {
-        Set<Integer> num_set = new HashSet<Integer>();
-        for (int num : nums) {
-            num_set.add(num);
+        HashSet<Integer> set = new HashSet<>();
+        
+        int maxLength = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            set.add(nums[i]);
         }
-
-        int longestStreak = 0;
-
-        for (int num : num_set) {
-            if (!num_set.contains(num-1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (num_set.contains(currentNum+1)) {
-                    currentNum += 1;
-                    currentStreak += 1;
+        for(int num:nums){
+            if(!set.contains(num-1)){
+                int count = 1;
+                int head = num;
+                while(set.contains(head+1)){
+                    head++;
+                    count++;
                 }
-
-                longestStreak = Math.max(longestStreak, currentStreak);
+                maxLength = Math.max(count,maxLength);
+                count = 1;
             }
         }
-
-        return longestStreak;
+        
+        
+        
+        return maxLength;
     }
 }
