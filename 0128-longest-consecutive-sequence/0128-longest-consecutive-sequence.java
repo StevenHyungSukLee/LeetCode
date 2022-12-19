d1109e741c6e6e2755f8810914992679b690fd92
StevenHyungSukLee <106793032+StevenHyungSukLee@users.noreply.github.com>
2022-12-18 21:31:31 -0500

    Time: 850 ms (10.37%), Space: 137.5 MB (23.78%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -27,10 +27,12 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         
         int maxLength = 0;
-        
-        for(int i = 0; i < nums.length; i++){
-            set.add(nums[i]);
+        for(int num : nums){
+            set.add(num);
         }
+        // for(int i = 0; i < nums.length; i++){
+        //     set.add(nums[i]);
+        // }
         for(int num:nums){
             if(!set.contains(num-1)){
                 int count = 1;
