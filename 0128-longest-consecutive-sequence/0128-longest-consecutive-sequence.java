013a338f1a2a24b4571e19669312ba7ea41f4b59
StevenHyungSukLee <106793032+StevenHyungSukLee@users.noreply.github.com>
2022-12-18 21:06:55 -0500

    Time: 379 ms (40.47%), Space: 59.2 MB (88.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -6,19 +6,6 @@ class Solution {
         int count = 1;
         for(int i = 0; i < nums.length; i++){
             set.add(nums[i]);
-            // if(map.containsKey(nums[i])){
-            //     continue;
-            // }else if(map.containsKey(nums[i]-1)){
-            //     map.get(nums[i]-1).add(nums[i]);
-            //     // map.put(nums[i]-1, nums[i]);
-            // }else if(map.containsKey(nums[i]+1)){
-            //     List<Integer> list = new ArrayList<>();
-            //     list.add(nums[i]+1);
-            //     map.put(nums[i], list);
-            // }else{
-            //     List<Integer> list = new ArrayList<>();
-            //     map.put(nums[i],list);
-            // }
         }
         for(int num:nums){
             if(!set.contains(num-1)){
