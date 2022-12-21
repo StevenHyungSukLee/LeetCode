25be7eaef70a1a46e536b615804fc35f712a708b
StevenHyungSukLee <106793032+StevenHyungSukLee@users.noreply.github.com>
2022-12-20 20:30:38 -0500

    Time: 1 ms (74.43%), Space: 43.2 MB (5.38%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -16,9 +16,9 @@ class Solution {
         ListNode res = head;
         ListNode temp = head;
         
-        if(head.next == null){
-            return null;
-        }
+        // if(head.next == null){
+        //     return null;
+        // }
         while(temp.next != null){
             count++;
             temp = temp.next;
